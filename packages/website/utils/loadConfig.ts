9e32e87a7c3da365ca90be1d84876f218483ec96
duhbb <duhbbx@gmail.com>
2024-05-24 07:49:48 +0800

    修复构建服务器的判断

--- packages/website/utils/loadConfig.ts
+++ packages/website/utils/loadConfig.ts
@@ -3,7 +3,9 @@ const normalizeURL = (url: string) => new URL(url).toString();
 // 从环境变量中读取.
 export const config = {
   baseUrl: normalizeURL(
-    process.env.VAN_BLOG_SERVER_URL ?? "http://localhost:3000"
+    process.env.VAN_BLOG_SERVER_URL && process.env.VAN_BLOG_SERVER_URL.trim() !== ""
+      ? process.env.VAN_BLOG_SERVER_URL
+      : "http://localhost:3000"
   ),
 };
 
